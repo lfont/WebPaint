6c555e87cfa4798ef27b630ac43253a2be4d49d2
Loïc Fontaine <ljph.fontaine@gmail.com>
2011-12-23 15:07:46 +0000

    Adding a simple handling of parameters to server.js.

--- server.js
+++ server.js
@@ -3,13 +3,29 @@ var http = require('http'),
     path = require('path'),
     fs = require('fs'),
     mime = require('mime'),
-    directory = '',
+    dir = '',
     host = '0.0.0.0',
     port = process.env.PORT;
     
+process.argv.forEach(function (val) {
+    var arg = val.split('=');
+    
+    switch (arg[0]) {
+        case 'dir':
+            dir = arg[1];
+            break;
+        case 'host':
+            host = arg[1];
+            break;
+        case 'port':
+            port = parseInt(arg[1], 10);
+            break;
+    }
+});
+
 http.createServer(function (req, res) {
     var uri = url.parse(req.url).pathname,
-        filename = path.join(process.cwd(), directory, uri);
+        filename = path.join(process.cwd(), dir, uri);
         
     path.exists(filename, function (exists) {
         var mimeType,
